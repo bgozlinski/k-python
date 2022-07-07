7a042905276f36d2ab81c57c8cbb588206c6af8a
bgozlinski <bartlomiej.gozlinski@gmail.com>
2022-07-08 00:47:15 +0200

    commit from pycharm

--- 20220626/annotacje.py
+++ 20220626/annotacje.py
@@ -1,7 +1,24 @@
 def suma(a: int, b: int) -> int:
+    """
+    Sum of two arguments
+    >>> suma(1, 2)
+    3
+
+    >>> suma(-1, -2)
+    -3
+
+    >>> suma(0, 0)
+    0
+    """
     return a+b
 
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
-print(suma(2,5))
 
-print(suma.__annotations__)
+# print(suma.__annotations__)
+# print(suma.__doc__)
+
+# python -m doctest .\annotacje.py
+# python .\annotacje.py -v
