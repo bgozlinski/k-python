a2f206c79267e8113d81747f77018a00cb33c3df
bgozlinski <bartlomiej.gozlinski@gmail.com>
2022-07-12 23:21:07 +0200

    homework

--- praca_domowa_20220711/zadanie_3/main.py
+++ praca_domowa_20220711/zadanie_3/main.py
@@ -1,17 +1,48 @@
+from gtts import gTTS
+import os
+from time import sleep, localtime, strftime
+import pyglet
+
+
 class Asystent:
-    def __init__(self, filename):
-        with open(filename, "r") as input_file:
-            self.file = input_file.read()
+    def __init__(self):
+        self.file_name = "config_file.txt"
+        self.temp_voice = "temp.mp3"
+        self.timeout = 2
 
     def read_file(self):
-        return self.file
+        with open(self.file_name, "r") as input_file:
+            file = input_file.read()
+            input_file.close()
+            return file
+
+    def speak(self, text):
+        tts = gTTS(text=text, lang="pl")
+        tts.save(self.temp_voice)
+
+        text_to_speach = pyglet.media.load(self.temp_voice, streaming=False)
+        text_to_speach.play()
+        sleep(self.timeout)
+        os.remove(self.temp_voice)
+
+    @staticmethod
+    def time_now():
+        t = localtime()
+        time_now = strftime("%H:%M", t)
+
+        return time_now
 
 
+if __name__ == "__main__":
+    assist = Asystent()
+    t = assist.time_now().replace(":", "")
+    print(t)
 
+    # assist.speak("przerwa obiadowa")
+    # assist.speak("czesc")
 
-def main():
-    a = Asystent("config_file.txt")
-    content = a.read_file()
-    print(content.split(' '))
+    file = assist.read_file().split()
+    a = file[0].split("-")
+    print(a[0].replace(":", ""))
 
-main()
+    print(a[0] < t)
