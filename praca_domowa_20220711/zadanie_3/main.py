744ff709dee7ba4b4a0af1429663e54104440809
bgozlinski <bartlomiej.gozlinski@gmail.com>
2022-07-13 03:21:47 +0200

    Asystent homework

--- praca_domowa_20220711/zadanie_3/main.py
+++ praca_domowa_20220711/zadanie_3/main.py
@@ -38,14 +38,14 @@ def file_convert(temp):
 
     #  merge two non-time strings
     for i in range(1, len(temp)):
-        if ':' not in (temp[i-1]) and ':' not in temp[i]:
+        if ":" not in (temp[i - 1]) and ":" not in temp[i]:
             break
-    temp[i-1 : i+1] = [' '.join(temp[i-1 : i+1])]
+    temp[i-1:i+1] = [" ".join(temp[i-1:i+1])]
 
     #  split two time-like strings
     new_temp = []
     for i in temp:
-        new_temp.append(i.split('-'))
+        new_temp.append(i.split("-"))
 
     return new_temp
 
@@ -54,23 +54,35 @@ if __name__ == "__main__":
     assist = Asystent()
 
     file = assist.read_file().split()
-    converted_file = (file_convert(file))
-    time_now = assist.time_now()
+    converted_file = file_convert(file)
+    previous_string_to_speak = ''
+    end = False
 
-    for i, j in enumerate(converted_file):
-        if i % 2 == 0:
-            if j[0] < time_now <= j[1]:
-                stop_list = i
-                stop_value = j
-                print(f'stop: {stop_list} value: {stop_value}')
-                break
-
-        else:
+    while True:
+        time_now = '09:00'
+        # time_now = assist.time_now()
+        if time_now >= converted_file[0][0]:
             break
 
-        if i is True:
-            string_to_speak = (converted_file[i+1])
-        else:
-            string_to_speak = 'Koniec'
-    print(string_to_speak)
-    assist.speak(string_to_speak)
+    while True:
+        time_now = '17:00'
+        # time_now = assist.time_now()
+        for i, j in enumerate(converted_file):
+            if ':' in j[0]:
+                if j[0] <= time_now < j[1]:
+                    stop_list = i
+                    stop_value = j
+                    end = False
+                    string_to_speak = converted_file[i+1][0]
+                    break
+
+                else:
+                    string_to_speak = 'Koniec'
+                    end = True
+
+        if end is True:
+            assist.speak(string_to_speak)
+            break
+        if previous_string_to_speak != string_to_speak:
+            assist.speak(string_to_speak)
+            previous_string_to_speak = string_to_speak
