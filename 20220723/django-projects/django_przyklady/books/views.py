ac6c457643bd19abf08607df617a37c650aae07b
bozlinski <bartlomiej.gozlinski@gmail.com>
2022-07-23 17:02:30 +0200

    django

--- 20220723/django-projects/django_przyklady/books/views.py
+++ 20220723/django-projects/django_przyklady/books/views.py
@@ -2,15 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+books = [
+    {'name': 'Pan Tadeusz', 'autor': 'Adam Mickiewicz', 'rok': '1834', 'ilosc_stron': '200'},
+    {'name': 'Harry Potter', 'autor': 'JKK Rowling', 'rok': '2000', 'ilosc_stron': '350'},
+    {'name': 'Hobbit', 'autor': 'JRR Tolkien', 'rok': '1937', 'ilosc_stron': '150'},
+    {'name': 'Przedwiośnie', 'autor': 'Stefan Żeromski', 'rok': '1924', 'ilosc_stron': '125'}
+]
 
 
 def listview(request):
-    books = [
-        {'name': 'Pan Tadeusz', 'autor': 'Adam Mickiewicz', 'rok': '1834', 'ilosc_stron': '200'},
-        {'name': 'Harry Potter', 'autor': 'JKK Rowling', 'rok': '2000', 'ilosc_stron': '350'},
-        {'name': 'Hobbit', 'autor': 'JRR Tolkien', 'rok': '1937', 'ilosc_stron': '150'},
-        {'name': 'Przedwiośnie', 'autor': 'Stefan Żeromski', 'rok': '1924', 'ilosc_stron': '125'}
-    ]
     return render(request, 'books.html', {'books': books})
 
 
